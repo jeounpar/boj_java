da959aa3390a3bf14499144892f9f4d1e462579c
jeounpar <cdex6531@ajou.ac.kr>
2022-05-06 02:05:34 +0900

    Solved

---
+++ BruteForceRecursion/BOJ_15651.java
@@ -0,0 +1,30 @@
+package BruteForceRecursion;
+
+import java.util.Scanner;
+
+public class BOJ_15651 {
+	static int[] arr = new int[10];
+	static StringBuilder sb = new StringBuilder();
+	static void solve(int index, int n, int m) {
+		if (index == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(arr[i]);
+				if (i + 1 != m)
+					sb.append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			arr[index] = i;
+			solve(index+1, n, m);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		solve(0, n, m);
+		System.out.print(sb);
+	}
+}
---
+++ BruteForceRecursion/BOJ_15652.java
@@ -0,0 +1,35 @@
+package BruteForceRecursion;
+
+import java.util.Scanner;
+
+public class BOJ_15652 {
+	static boolean[] ok = new boolean[10];
+	static int[] a = new int[10];
+	static StringBuilder sb = new StringBuilder();
+	static void solve(int index, int pre, int n, int m) {
+		if (index == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(a[i]);
+				if (i + 1 != m)
+					sb.append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			if (i < pre)
+				continue;
+			ok[i] = true;
+			a[index] = i;
+			solve(index+1, i, n, m);
+			ok[i] = false;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		solve(0, 0, n, m);
+		System.out.print(sb);
+	}
+}
